ab2496c241d24d8111179aec79622fa43a135329
rreggiar <rreggiar@ucsc.edu>
2020-08-19 11:46:37 -0700

    updating adapter location

--- scripts/qc.trim.salmon.star.sh
+++ scripts/qc.trim.salmon.star.sh
@@ -6,7 +6,7 @@ TXINDEX='/public/groups/kimlab/indexes/gencode.32.v.1.index/'
 #TXINDEX='/public/groups/kimlab/indexes/te.locus.v.1.index/'
 HG38='/public/groups/kimlab/genomes.annotations/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna'
 GENOMEDIR='/public/groups/kimlab/genomes.annotations/HG.38.w.te.cons/'
-ADAPTERS='/public/home/rreggiar/genomes.annotations/adapters'
+ADAPTERS='/public/groups/kimlab/genomes.annotations/adapters'
 OUTDIR=gencode.salmon.out
 
 STAR --version
