b44781e22299cb35dea9f3a658f529ba0fb6614f
rreggiar <rreggiar@ucsc.edu>
2020-08-21 16:58:37 -0700

    running on exo data with custom alu ref

--- scripts/exo.rna.editing.docker.fun.sh
+++ scripts/exo.rna.editing.docker.fun.sh
@@ -1,10 +1,10 @@
 
-for BEDFILE in sleuth.alu.bed; do
+for BEDFILE in exo.alu.bed; do
 #for BEDFILE in gencode.as1.bed kras.alu.bed kras.mer.bed sz.sx.sx1.y.bed; do
   docker run \
     -u $(id -u ${USER}):$(id -g ${USER})\
-    -v '/public/groups/kimlab/aale.luad.exo/aale.exo.data/bam.files':'/data/input_files'\
-    -v '/public/groups/kimlab/aale.kras/data/bulk.rna.seq/aale/output/exo.sleuth.alu.edit.out':'/data/output_dir'\
+    -v '/public/groups/kimlab/aale.luad.exo/bam.files':'/data/input_files'\
+    -v '/public/groups/kimlab/aale.kras/data/bulk.rna.seq/aale/output/exo.alu.edit.out':'/data/output_dir'\
     rna_editing:1.0 RNAEditingIndex -d '/data/input_files/'\
     -l '/data/output_dir/log.files' -o '/data/output_dir/cmpileups'\
     -os '/data/output_dir/summary_dir'\
