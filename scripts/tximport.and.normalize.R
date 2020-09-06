6aa9adf73417266093e2fffbc607af81d4217814
rreggiar <rreggiar@ucsc.edu>
2020-09-06 16:38:51 -0700

    tx out T

--- scripts/tximport.and.normalize.R
+++ scripts/tximport.and.normalize.R
@@ -58,7 +58,7 @@ names(tx2gene) <- c("Name", "GeneID")
 txi <- tximport(files, 
                 type="salmon", 
                 tx2gene = tx2gene,
-                txOut = F)
+                txOut = T)
 
 print(head(txi$counts))
 
