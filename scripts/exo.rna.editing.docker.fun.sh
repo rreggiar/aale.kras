7beac0ef6dd46e4faafde5c5efe470caac459abc
rreggiar <rreggiar@ucsc.edu>
2020-09-08 16:05:12 -0700

    exotic editing, added stdin

--- scripts/exo.rna.editing.docker.fun.sh
+++ scripts/exo.rna.editing.docker.fun.sh
@@ -4,7 +4,7 @@ for BEDFILE in *.bed; do
 #for BEDFILE in gencode.as1.bed kras.alu.bed kras.mer.bed sz.sx.sx1.y.bed; do
   docker run \
     -u $(id -u ${USER}):$(id -g ${USER})\
-    -v '/public/groups/kimlab/aale.kras/data/bulk.rna.seq/exotic/output/exotic.alu.edit.out':'/data/input_files'\
+    -v '/public/groups/kimlab/aale.kras/data/bulk.rna.seq/exotic/output/exotic.alu.edit.in':'/data/input_files'\
     -v '/public/groups/kimlab/aale.kras/data/bulk.rna.seq/exotic/output/exotic.alu.edit.out':'/data/output_dir'\
     rna_editing:1.0 RNAEditingIndex -d '/data/input_files/'\
     -l '/data/output_dir/log.files' -o '/data/output_dir/cmpileups'\
