b322c3271556dc3e72ce1351968a231eee972f1b
rreggiar <rreggiar@ucsc.edu>
2020-09-08 14:15:28 -0700

    switch to bash naming syntax from R

--- scripts/alu.editing.wrapper.sh
+++ scripts/alu.editing.wrapper.sh
@@ -2,13 +2,14 @@
 
 # wrapper script to generate alu loci for editing
 
-input.counts.file=$1
-output.tmp.counts=$2
-output.locus.bed=$3
+input_counts_file=$1
+echo ""
+output_tmp_counts=$2
+output_locus_bed=$3
 
-echo "$input.counts.file" "$output.tmp.counts" | Rscript alu.loci.for.editing.R
+echo "$input_counts_file" "$output_tmp_counts" | Rscript alu.loci.for.editing.R
 
-until [ -f "$output.tmp.counts" ]
+until [ -f "$output_tmp_counts" ]
 
 do
 	sleep 5
@@ -16,6 +17,6 @@ done
 
 echo "creating bed file"
 
-cut -d"'" -f1 "$output.tmp.counts" | cut -d'=' -f2 | sed 's/:/\t/g ; s/-/\t/g ; s/_5//g' | sort -k1,1 -2,2n > "$output.locus.bed"
+cut -d"'" -f1 "$output_tmp_counts" | cut -d'=' -f2 | sed 's/:/\t/g ; s/-/\t/g ; s/_5//g' | sort -k1,1 -2,2n > "$output_locus_bed"
 
-rm "$output.tmp.counts"
+rm "$output_tmp_counts"
