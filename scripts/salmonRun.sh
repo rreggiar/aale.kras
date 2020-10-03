be3fa0947227aeca0f7dacd72f5aa3916b18dae4
rreggiar <rreggiar@ucsc.edu>
2020-10-03 15:48:14 -0700

    conda loop done

--- scripts/salmonRun.sh
+++ scripts/salmonRun.sh
@@ -2,7 +2,9 @@
 
 # rreggiar@ucsc.edu
 # conda -- aale.analysis.env
+
 ## activate correct env
+# source the conda script so this shell has access
 source /public/groups/kimlab/.install_bin/anaconda3/etc/profile.d/conda.sh
 
 reqEnv="aale.analysis.env"
@@ -11,4 +13,7 @@ env=$(basename "$CONDA_PREFIX")
 if [[ env != reqEnv ]]; then
 	echo "switching from "$env" to "$reqEnv""
 	conda activate $reqEnv
+else
+	echo ""$reqEnv" is active"
 fi
+
