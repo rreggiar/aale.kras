9b9197e94eacbe74b11beb6e671673b3dc2fc721
rreggiar <rreggiar@ucsc.edu>
2020-10-05 11:14:01 -0700

    pipeline wrapper

---
+++ scripts/rnaSeqPipeLine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#!/bin/bash
+
+# rreggiar@ucsc.edu
+
+
+scriptName=$(basename $0)
+if [ $# -lt 7 ]; then
+    echo "error: usage $scriptName trim=T salmon=T star=T inputDir salmonIndex starGenome adapterChoice "
+    echo "example $scriptName "
+    exit 1
+fi
+
+modules=('trimmomaticRun.sh' 'salmonRun.sh' 'starRun.sh')
+
+for module in modules; do
+	if [[ -f "$module" ]]; then
+		echo "$module" "is present"
+	else
+		echo "$module" "is absent"
+		exit 1
+	fi
+done
+
+dateStamp="$(bash dateStamp.sh)"
+echo "script: $scriptName"
+echo "time: $dateStamp"
