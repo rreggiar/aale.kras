953987255963b23255dfc526a9681b07462d9ebf
rreggiar <rreggiar@ucsc.edu>
2020-10-05 21:08:11 -0700

    added single end support

--- scripts/trimmomaticRun.sh
+++ scripts/trimmomaticRun.sh
@@ -58,20 +58,21 @@ echo "trimmomatic:" $(trimmomatic -version)
 # I think using directories as envNames could make this moot
 # OR allow us to rely on "$(basename $PWD)" as $reqENV which would make the check
 # super portable
-function condaCheck() {
-	# source the conda script so this shell has access
-	source /public/groups/kimlab/.install_bin/anaconda3/etc/profile.d/conda.sh
+# rerwip -- move this to util script
+# function condaCheck() {
+# 	# source the conda script so this shell has access
+# 	source /public/groups/kimlab/.install_bin/anaconda3/etc/profile.d/conda.sh
 
-	reqEnv="aale.analysis.env"
-	env=$(basename "$CONDA_PREFIX")
+# 	reqEnv="aale.analysis.env"
+# 	env=$(basename "$CONDA_PREFIX")
 
-	if [[ env != reqEnv ]]; then
-		echo "switching from "$env" to "$reqEnv""
-		conda activate $reqEnv
-	else
-		echo ""$reqEnv" is active"
-	fi
-}
+# 	if [[ env != reqEnv ]]; then
+# 		echo "switching from "$env" to "$reqEnv""
+# 		conda activate $reqEnv
+# 	else
+# 		echo ""$reqEnv" is active"
+# 	fi
+# }
 
 ## since trimming is deterministic and reproducible, I'm going to treat these as un-dated
 ## output files for now so they can be accessed by various tools/pipelines and not be needlessly
@@ -83,49 +84,90 @@ function runTrimmomatic() {
 	inputDir="$1"
 
 	# check for existence of canonical output file name
-	if [ ! -f "$inputDir"/output_forward_paired.fq.gz ]; then
-		# if the read files exist and are symlinks
-		if [[ -L "$inputDir"/*_R1_001.fastq.gz ]]; then
-			# assign the reads  -- prepping for move to $SCRATCH data directories
-			read_1=$(readlink "$inputDir"/*_R1_001.fastq.gz)
-			read_2=$(readlink "$inputDir"/*_R2_001.fastq.gz)
+	if [[ ! -f "$inputDir"/output_forward_paired.fq.gz ]]; then
+		# look for second paired read, if that exists approach as paired library
+		if [[ -f "$inputDir"/*_2*.fastq.gz ]]; then
+			#statements
+			# if the read files exist and are symlinks
+			if [[ -L "$inputDir"/*_1*.fastq.gz ]]; then
+				# assign the reads  -- prepping for move to $SCRATCH data directories
+				read_1=$(readlink "$inputDir"/*_R1_001.fastq.gz)
+				read_2=$(readlink "$inputDir"/*_R2_001.fastq.gz)
+			else
+				read_1="$inputDir"/*_1*.fastq.gz
+				read_2="$inputDir"/*_2*.fastq.gz
+			fi
+			
+			set -x
+			echo "read 1:" "$read_1"
+			echo "read 2:" "$read_2"
+			echo "Trimming fastq"
+
+			# run trimm with recommended arguments
+			trimmomatic PE  -threads 8 \
+				"$read_1" "$read_2" \
+				"$inputDir"/output_forward_paired.fq.gz \
+				"$inputDir"/output_forward_unpaired.fq.gz \
+				"$inputDir"/output_reverse_paired.fq.gz \
+				"$inputDir"/output_reverse_unpaired.fq.gz \
+				ILLUMINACLIP:"$adapters"/"$adapterChoice":1:30:10:4:true \
+				LEADING:3 \
+				TRAILING:3 \
+				SLIDINGWINDOW:4:15 \
+				MINLEN:36
+
+			exitStatus=$?
+			if [ $? -ne 0 ]; then
+
+			    echo ERROR trimmomatic "$inputDir" returned exit status "$exitStatus"
+			    continue
+
+			fi
+
 		else
-			read_1="$inputDir"/*_R1_001.fastq.gz
-			read_2="$inputDir"/*_R2_001.fastq.gz
+			# if read-pair not found, use single end arguments
+
+			if [[ -L "$inputDir"/*_1*.fastq.gz ]]; then
+				# assign the reads  -- prepping for move to $SCRATCH data directories
+				read_1=$(readlink "$inputDir"/*_R1_001.fastq.gz)
+				#read_2=$(readlink "$inputDir"/*_R2_001.fastq.gz)
+			else
+				read_1="$inputDir"/*_1*.fastq.gz
+				#read_2="$inputDir"/*_2*.fastq.gz
+			fi
+			
+			set -x
+			echo "read 1:" "$read_1"
+			# echo "read 2:" "$read_2"
+			echo "Trimming fastq"
+
+			# run trimm with recommended arguments
+			trimmomatic PE  -threads 8 \
+				"$read_1"  \
+				"$inputDir"/output_forward_paired.fq.gz \
+				"$inputDir"/output_forward_unpaired.fq.gz \
+				ILLUMINACLIP:"$adapters"/"$adapterChoice":1:30:10:4:true \
+				LEADING:3 \
+				TRAILING:3 \
+				SLIDINGWINDOW:4:15 \
+				MINLEN:36
+
+
+			exitStatus=$?
+			if [ $? -ne 0 ]; then
+
+			    echo ERROR trimmomatic "$inputDir" returned exit status "$exitStatus"
+			    continue
+
+			fi	
 		fi
-		
-		set -x
-		echo "read 1:" "$read_1"
-		echo "read 2:" "$read_2"
-		echo "Trimming fastq"
-
-		# run trimm with recommended arguments
-		trimmomatic PE  -threads 8 \
-			"$read_1" "$read_2" \
-			"$inputDir"/output_forward_paired.fq.gz \
-			"$inputDir"/output_forward_unpaired.fq.gz \
-			"$inputDir"/output_reverse_paired.fq.gz \
-			"$inputDir"/output_reverse_unpaired.fq.gz \
-			ILLUMINACLIP:"$adapters"/"$adapterChoice":1:30:10:4:true \
-			LEADING:3 \
-			TRAILING:3 \
-			SLIDINGWINDOW:4:15 \
-			MINLEN:36
-
-		exitStatus=$?
-		if [ $? -ne 0 ]; then
-
-		    echo ERROR trimmomatic "$inputDir" returned exit status "$exitStatus"
-		    continue
-
-		fi	
 
 		set +x
 
 	fi
 }
 
-condaCheck
+# condaCheck
 
 runTrimmomatic "${inputDir}" # specify file path
 
