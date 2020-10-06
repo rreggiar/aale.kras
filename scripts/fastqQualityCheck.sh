f024c326ae3679d5e2cf82aa78addf291ee5dd75
rreggiar <rreggiar@ucsc.edu>
2020-10-06 13:08:58 -0700

    init for fastqc and multiqc wrapper

---
+++ scripts/fastqQualityCheck.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# rreggiar@ucsc.edu
+
+
+scriptName=$(basename $0)
+if [ $# -lt 3 ]; then
+    echo "error: usage "$scriptName" sampleDir"
+    echo "example "$scriptName" /scratch/kimlab/projects/exoRNA-biomarkers-panc/data/a549_0.2MOI_24hr"
+    exit 1
+fi
+
+sampleDir="$1"
+
+dateStamp="$(bash dateStamp.sh)"
+
+echo "cmd: "$@""
+
+for inputDir in "$sampleDir"/*; do
+
+	set -x 
+
+	fastqc -t 8 output*
+
+	exitStatus=$?
+	if [ $? -ne 0 ]; then
+
+	    echo ERROR "$scriptName" "$inputDir" returned exit status "$exitStatus"
+	    continue
+
+	fi
+
+	set +x
+
+done
+
+multiqc "$sampleDir" --ingore *.fastq.gz --filename multiqc."${dateStamp}" -outdir "$sampleDir"
